241a0d48a6500a19c406ee9711c84956567c8ceb
doug.barthram <doug.barthram@lab49.com>
2015-09-01 19:33:43 +1000

    Load the schedule data from file (over http)

--- lights-webclient/app/js/controllers.js
+++ lights-webclient/app/js/controllers.js
@@ -4,39 +4,12 @@
 
 var lightsApp = angular.module('lightsApp', []);
 
-lightsApp.controller('ScheduleListCtrl', function($scope) {
-    $scope.schedules = [
+lightsApp.controller('ScheduleListCtrl', ['$scope', '$http',
+    function($scope, $http) {
 
-        {
-            'name': 'summer',
-            'description': 'Schedule for a nice summers night',
-            'lights': [
-                {'id': '0', 'wait': '60', 'onFor': '180'},
-                {'id': '1', 'wait': '10', 'onFor': '120'},
-                {'id': '2', 'wait': '30', 'onFor': '210'},
-                {'id': '3', 'wait': '00', 'onFor': '180'}
-            ]
-        },
-        {
-            'name': 'winter',
-            'description': 'Schedule for a winter night',
-            'lights': [
-                {'id': '0', 'wait': '00', 'onFor': '240'},
-                {'id': '1', 'wait': '10', 'onFor': '240'},
-                {'id': '2', 'wait': '30', 'onFor': '210'},
-                {'id': '3', 'wait': '00', 'onFor': '180'}
-            ]
-        }
-    ];
-});
-//lightControllers.controller('ScheduleListCtrl', ['$scope', '$http',
-//    function($scope, $http) {
-//
-//
-//
-//        $http.get('schedules/all_schedules.json').success(function(data) {
-//            $scope.schedules = data;
-//            alert("loaded schedules")
-//        });
-//
-//    }]);
+        $http.get('schedules/all_schedules.json').success(function(data) {
+            $scope.schedules = data;
+        });
+
+    }]);
+
