728465baa162db4b4f2be8cced5cd911049e96a0
doug.barthram <doug.barthram@lab49.com>
2015-09-07 16:36:07 +1000

    Added a load of files that got missed

--- lights-webclient/app/js/services.js
+++ lights-webclient/app/js/services.js
@@ -1,11 +1,5 @@
 var lightServices = angular.module('lightServices', ['ngResource']);
 
-//lightServices.factory('Schedules', ['$resource',
-//    function($resource){
-//        return $resource('http://localhost:8080/schedules/:id');
-//    }]);
-
-
 
 lightServices.factory('Schedules', ['$resource',
     function($resource){
